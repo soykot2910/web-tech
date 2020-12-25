99b6b8f2239ddb447b3e9136baa41995dc7538c4
soykot2910 <md.soykot2910@gmail.com>
2020-12-25 23:39:06 +0600

    responsive done

--- assignment/assignment-5/app.js
+++ assignment/assignment-5/app.js
@@ -4,6 +4,7 @@ window.onscroll = function () {
   scrollFunction();
 };
 
+
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("navbar").style.height = "8vh";
